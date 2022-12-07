7d56989b1f80988967614e89b490600dcdece22a
Michael Gostner <1592923+migos@users.noreply.github.com>
2022-12-07 12:06:32 +0100

    Add more dimensions, remove Dimensions value

--- base-ui/src/main/kotlin/com/tailoredapps/androidapptemplate/base/ui/theme/Dimensions.kt
+++ base-ui/src/main/kotlin/com/tailoredapps/androidapptemplate/base/ui/theme/Dimensions.kt
@@ -7,17 +7,20 @@ import androidx.compose.ui.unit.dp
  * https://material.io/design/layout/understanding-layout.html#material-measurements
  */
 class Dimensions(
-    val dimen_4: Dp = 4.dp,
-    val dimen_8: Dp = 8.dp,
-    val dimen_16: Dp = 16.dp,
-    val dimen_24: Dp = 24.dp,
-    val dimen_32: Dp = 32.dp,
-    val dimen_40: Dp = 40.dp,
-    val dimen_48: Dp = 48.dp,
-    val dimen_56: Dp = 56.dp,
-    val dimen_64: Dp = 64.dp,
-    val dimen_72: Dp = 72.dp,
-    val dimen_80: Dp = 80.dp
+    val dimen1: Dp = 1.dp,
+    val dimen2: Dp = 2.dp,
+    val dimen4: Dp = 4.dp,
+    val dimen6: Dp = 6.dp,
+    val dimen8: Dp = 8.dp,
+    val dimen16: Dp = 16.dp,
+    val dimen24: Dp = 24.dp,
+    val dimen32: Dp = 32.dp,
+    val dimen40: Dp = 40.dp,
+    val dimen48: Dp = 48.dp,
+    val dimen56: Dp = 56.dp,
+    val dimen64: Dp = 64.dp,
+    val dimen72: Dp = 72.dp,
+    val dimen80: Dp = 80.dp
 )
 
 val smallDimensions = Dimensions()
